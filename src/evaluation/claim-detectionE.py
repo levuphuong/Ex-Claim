2776aaab3e3c11cf3c2956ae4ad1935a32930610
Phuong Le <levuphuong07@gmail.com>
2026-01-14 20:55:19 +0700

    Update

--- src/evaluation/claim-detectionE.py
+++ src/evaluation/claim-detectionE.py
@@ -6,8 +6,8 @@ import src.util.Training as Training
 import src.models.ClaimDetection as CD_Models
 import src.util.Evaluation as Evaluation
 import src.util.NER as NER
-import src.util.EntityLinking as EntityLinking
-from genre.trie import Trie, MarisaTrie
+# import src.util.EntityLinking as EntityLinking
+# from genre.trie import Trie, MarisaTrie
 import src.util.Results as Results
 
 task = "verifiable-claim-detection"
@@ -31,7 +31,7 @@ output_size = 2
 learning_rate = 3e-5
 epochs = 30
 MODELS_PATH = path + "Models/Claim-Detection/"
-iterations = 2
+iterations = 10
 training_languages = ["english", "dutch", "bulgarian", "turkish"]
 we_size = Training.getInputSize(word_embeddings)
 ee_size = 256
@@ -62,38 +62,38 @@ for i in range(iterations):
                                           "test", path)
 Results.computeAveragePerformance(path, "test")
 logging.info(f"Completed training {model_prefix}")
-########################################################################################################################
-ee_size = 256
-# Wiki entity extraction using coarse-grained NER
-wiki_entity_scores_cg = EntityLinking.getWikiEntityPresenceScore(path, ner_tagged_sentences_cg, ner_vectors_cg)
+# ########################################################################################################################
+# ee_size = 256
+# # Wiki entity extraction using coarse-grained NER
+# wiki_entity_scores_cg = EntityLinking.getWikiEntityPresenceScore(path, ner_tagged_sentences_cg, ner_vectors_cg)
 
-wiki_entity_presence_threshold = -0.15
-entity_indexes, no_entityCW = EntityLinking.getELIndexVector(entity_indexes, wiki_entity_scores_cg,
-                                                             wiki_entity_presence_threshold, no_entityC)
+# wiki_entity_presence_threshold = -0.15
+# entity_indexes, no_entityCW = EntityLinking.getELIndexVector(entity_indexes, wiki_entity_scores_cg,
+#                                                              wiki_entity_presence_threshold, no_entityC)
 
 
-train_loader = Training.getDataLoaderWithEntityData(word_embeddings, entity_indexes, class_labels, training_languages,
-                                                    ["train"], batch_size, shuffle=True)
-validation_loader = Training.getDataLoaderWithEntityData(word_embeddings, entity_indexes, class_labels,
-                                                         training_languages, ["dev"], batch_size,
-                                                         shuffle=False)
+# train_loader = Training.getDataLoaderWithEntityData(word_embeddings, entity_indexes, class_labels, training_languages,
+#                                                     ["train"], batch_size, shuffle=True)
+# validation_loader = Training.getDataLoaderWithEntityData(word_embeddings, entity_indexes, class_labels,
+#                                                          training_languages, ["dev"], batch_size,
+#                                                          shuffle=False)
 
-########################################################################################################################
-# Word embedding + NER + Wiki Entity Presence
-model_prefix = "EClassifier-NER-cWiki" + str(int(wiki_entity_presence_threshold * 100)) + "-" + run_name
-logging.info(f"Started training {model_prefix}")
+# ########################################################################################################################
+# # Word embedding + NER + Wiki Entity Presence
+# model_prefix = "EClassifier-NER-cWiki" + str(int(wiki_entity_presence_threshold * 100)) + "-" + run_name
+# logging.info(f"Started training {model_prefix}")
 
-for i in range(iterations):
-    model_name = model_prefix + "-" + str(i) + ".pth"
-    model = CD_Models.EXClaim(we_size, ee_size, no_entityCW, hidden_size, output_size)
-    Training.trainModelWithEntityData(model, train_loader, validation_loader, learning_rate, epochs,
-                                      MODELS_PATH + model_name)
+# for i in range(iterations):
+#     model_name = model_prefix + "-" + str(i) + ".pth"
+#     model = CD_Models.EXClaim(we_size, ee_size, no_entityCW, hidden_size, output_size)
+#     Training.trainModelWithEntityData(model, train_loader, validation_loader, learning_rate, epochs,
+#                                       MODELS_PATH + model_name)
 
-    best_model = Training.loadModel(model, MODELS_PATH + model_name)
-    Evaluation.evaluateTestDataWithEntity(best_model, model_name, word_embeddings, entity_indexes, dataset,
-                                          "test", path)
-Results.computeAveragePerformance(path, "test")
-logging.info(f"Completed training {model_prefix}")
+#     best_model = Training.loadModel(model, MODELS_PATH + model_name)
+#     Evaluation.evaluateTestDataWithEntity(best_model, model_name, word_embeddings, entity_indexes, dataset,
+#                                           "test", path)
+# Results.computeAveragePerformance(path, "test")
+# logging.info(f"Completed training {model_prefix}")
 
 ########################################################################################################################
 # Coarse-grained NER models completed. Clear all the variables
@@ -136,43 +136,43 @@ for i in range(iterations):
 Results.computeAveragePerformance(path, "test")
 logging.info(f"Completed training {model_prefix}")
 
-########################################################################################################################
+# ########################################################################################################################
 
-# Clear all the variables
-model = None
-best_model = None
-train_loader = None
-validation_loader = None
-Util.clearMemory()
+# # Clear all the variables
+# model = None
+# best_model = None
+# train_loader = None
+# validation_loader = None
+# Util.clearMemory()
 
-########################################################################################################################
-ee_size = 256
-# Wiki entity extraction using fine-grained NER
-wiki_entity_scores_fg = EntityLinking.getWikiEntityPresenceScore(path, ner_tagged_sentences_fg, ner_vectors_fg)
+# ########################################################################################################################
+# ee_size = 256
+# # Wiki entity extraction using fine-grained NER
+# wiki_entity_scores_fg = EntityLinking.getWikiEntityPresenceScore(path, ner_tagged_sentences_fg, ner_vectors_fg)
 
-wiki_entity_presence_threshold = -0.15
-entity_indexes, no_entityFW = EntityLinking.getELIndexVector(entity_indexes, wiki_entity_scores_fg,
-                                                             wiki_entity_presence_threshold, no_entityF)
+# wiki_entity_presence_threshold = -0.15
+# entity_indexes, no_entityFW = EntityLinking.getELIndexVector(entity_indexes, wiki_entity_scores_fg,
+#                                                              wiki_entity_presence_threshold, no_entityF)
 
-train_loader = Training.getDataLoaderWithEntityData(word_embeddings, entity_indexes, class_labels, training_languages,
-                                                    ["train"], batch_size, shuffle=True)
-validation_loader = Training.getDataLoaderWithEntityData(word_embeddings, entity_indexes, class_labels,
-                                                         training_languages, ["dev"], batch_size,
-                                                         shuffle=False)
+# train_loader = Training.getDataLoaderWithEntityData(word_embeddings, entity_indexes, class_labels, training_languages,
+#                                                     ["train"], batch_size, shuffle=True)
+# validation_loader = Training.getDataLoaderWithEntityData(word_embeddings, entity_indexes, class_labels,
+#                                                          training_languages, ["dev"], batch_size,
+#                                                          shuffle=False)
 
-########################################################################################################################
-# Word embedding + NER + Wiki Entity Presence
-model_prefix = "EClassifier-NER-fWiki" + str(int(wiki_entity_presence_threshold * 100)) + "-" + run_name
-logging.info(f"Started training {model_prefix}")
+# ########################################################################################################################
+# # Word embedding + NER + Wiki Entity Presence
+# model_prefix = "EClassifier-NER-fWiki" + str(int(wiki_entity_presence_threshold * 100)) + "-" + run_name
+# logging.info(f"Started training {model_prefix}")
 
-for i in range(iterations):
-    model_name = model_prefix + "-" + str(i) + ".pth"
-    model = CD_Models.EXClaim(we_size, ee_size, no_entityFW, hidden_size, output_size)
-    Training.trainModelWithEntityData(model, train_loader, validation_loader, learning_rate, epochs,
-                                      MODELS_PATH + model_name)
+# for i in range(iterations):
+#     model_name = model_prefix + "-" + str(i) + ".pth"
+#     model = CD_Models.EXClaim(we_size, ee_size, no_entityFW, hidden_size, output_size)
+#     Training.trainModelWithEntityData(model, train_loader, validation_loader, learning_rate, epochs,
+#                                       MODELS_PATH + model_name)
 
-    best_model = Training.loadModel(model, MODELS_PATH + model_name)
-    Evaluation.evaluateTestDataWithEntity(best_model, model_name, word_embeddings, entity_indexes, dataset,
-                                          "test", path)
-Results.computeAveragePerformance(path, "test")
-logging.info(f"Completed training {model_prefix}")
+#     best_model = Training.loadModel(model, MODELS_PATH + model_name)
+#     Evaluation.evaluateTestDataWithEntity(best_model, model_name, word_embeddings, entity_indexes, dataset,
+#                                           "test", path)
+# Results.computeAveragePerformance(path, "test")
+# logging.info(f"Completed training {model_prefix}")
